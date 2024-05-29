c80360f50ebe2c807679c4f86ca6915b5a960382
Akash Sharma <aakash6263264@gmail.com>
2024-05-29 11:23:13 +0530

    fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const User = require("./models/User");
 // Setup Express
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "https://online-status-indicator.vercel.app/" }));
 const server = http.createServer(app);
 const io = socketIo(server, {
